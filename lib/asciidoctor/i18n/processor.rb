589a0b743ed5c723f6b4d80b84a6411a8bb03ed8
Seiichi KONDO <seikichi@kmc.gr.jp>
2018-01-15 23:00:41 -0800

    Support basic asciidoctor syntax

--- lib/asciidoctor/i18n/processor.rb
+++ lib/asciidoctor/i18n/processor.rb
@@ -9,56 +9,65 @@ module Asciidoctor
   module I18n
     class Processor < Extensions::Treeprocessor
       def process(document)
-        locales = document.attr(:locales, 'locales')
         language = document.attr(:language, ENV['LANG'][0..1])
-        po = GetText::POParser.new.parse(File.read(File.join(locales, "#{language}.po")), GetText::PO.new)
-
-        process_document(document, po)
+        po_path = File.join(document.attr(:po_directory, 'po_directory'), "#{language}.po")
+        old_po = GetText::PO.new
+        new_po = GetText::PO.new
+        old_po = GetText::POParser.new.parse(File.read(po_path), old_po) if File.exist?(po_path)
+        process_document(document, old_po, new_po)
+        File.write(po_path, new_po.to_s)
       end
 
-      def process_document(document, po)
+      def process_document(document, old_po, new_po)
         document.find_by.each do |src|
-          case src.context
-          when :paragraph then process_paragraph(src, po)
-          when :section then process_section(src, po)
-          when :list_item then process_list_item(src, po)
-          when :table then process_table(src, po)
-          else puts src.context
-          end
+          process_abstract_block(src, old_po, new_po) if src.is_a?(Asciidoctor::AbstractBlock)
+          process_block(src, old_po, new_po) if src.is_a?(Asciidoctor::Block)
+          process_table(src, old_po, new_po) if src.is_a?(Asciidoctor::Table)
+          process_list_item(src, old_po, new_po) if src.is_a?(Asciidoctor::ListItem)
         end
       end
 
       private
 
-      def process_section(src, po)
-        return unless po.has_key?(src.title)
-        src.title = po[src.title].msgstr
-      end
-
-      def process_table(src, po)
-        process_section(src, po)
-
+      def process_table(src, old_po, new_po)
         src.rows.body.each do |row|
           row.each do |cell|
-            if cell.style == nil
-              process_list_item(cell, po)
-            else
-              process_document(cell.inner_document, po)
-            end
+            process_table_cell(cell, old_po, new_po)
           end
         end
       end
 
-      def process_paragraph(src, po)
-        src.lines = src.lines.map do |line|
-          po.has_key?(line) ? po[line].msgstr : line
+      def process_list_item(src, old_po, new_po)
+        text = src.instance_variable_get(:@text)
+        value = msgstr(text, old_po, new_po)
+        src.text = value if value != ''
+      end
+
+      def process_table_cell(src, old_po, new_po)
+        if src.style != :asciidoc
+          text = src.instance_variable_get(:@text)
+          value = msgstr(text, old_po, new_po)
+          src.text = value if value != ''
+        else
+          process_document(src.inner_document, old_po, new_po)
         end
       end
 
-      def process_list_item(src, po)
-        text = src.instance_variable_get(:@text)
-        return unless po.has_key?(text)
-        src.text = po[text].msgstr
+      def process_block(src, old_po, new_po)
+        text = src.lines.join("\n")
+        value = msgstr(text, old_po, new_po)
+        src.lines = value != '' ? value.split("\n") : src.lines
+      end
+
+      def process_abstract_block(src, old_po, new_po)
+        return unless src.title
+        value = msgstr(src.title, old_po, new_po)
+        src.title = value if value != ''
+      end
+
+      def msgstr(key, old_po, new_po)
+        new_po[key] = old_po.has_key?(key) ? old_po[key].msgstr : ''
+        new_po[key].msgstr || ''
       end
     end
   end
--- test/test_processor.rb
+++ test/test_processor.rb
@@ -5,14 +5,64 @@ require 'asciidoctor/i18n/processor'
 require 'gettext/po'
 require 'asciidoctor'
 
-class TestParagraph < Test::Unit::TestCase
-  test 'paragraph is translated correctly' do
-    po = GetText::PO.new
-    po['Hello, world!'] = 'こんにちは、世界！'
-    document = Asciidoctor.load('Hello, world!')
-    Asciidoctor::I18n::Processor.new.process_document(document, po)
+class TestProcessor < Test::Unit::TestCase
+  setup do
+    @po = GetText::PO.new
+    @po['Hello'] = 'こんにちは'
+    @po["Hello +\nHello"] = "こんにちは +\nこんにちは"
+    @po['*bold*, _italic phrase_, `monospace phrase`'] = '*太字*、_イタリックのフレーズ_、`モノスペースのフレーズ`'
+    @new_po = GetText::PO.new
+    @processor = Asciidoctor::I18n::Processor.new
+  end
 
-    para = document.find_by(context: :paragraph).first
-    assert { para && para.content == 'こんにちは、世界！' }
+  data(
+    'paragraph' => ['Hello', :paragraph, :source, 'こんにちは'],
+    'literal' => [' Hello', :literal, :source, 'こんにちは'],
+    'line breaks' => ["Hello +\nHello", :paragraph, :source, "こんにちは +\nこんにちは"],
+    'admonition' => ['NOTE: Hello', :admonition, :source, 'こんにちは'],
+    'lead paragraph' => ["[.lead]\nHello", :paragraph, :source, 'こんにちは'],
+    'bold, italic and monospace' => [
+      '*bold*, _italic phrase_, `monospace phrase`',
+      :paragraph,
+      :source,
+      '*太字*、_イタリックのフレーズ_、`モノスペースのフレーズ`',
+    ],
+    'section' => ['= Hello', :section, :title, 'こんにちは'],
+    'unordered list item' => ['* Hello', :list_item, :text, 'こんにちは'],
+    'ordered list item' => ['. Hello', :list_item, :text, 'こんにちは'],
+    'check list item' => ['* [*] Hello', :list_item, :text, 'こんにちは'],
+    'labeled single line item' => ['Hello:: Hello', :list_item, :text, 'こんにちは'],
+    'labeled multi line item' => ["Hello::\n  Hello", :list_item, :text, 'こんにちは'],
+    'sidebar' => [['.Hello', '****', 'Hello', '****'].join("\n"), :sidebar, :title, 'こんにちは'],
+    'blockquote' => [['____', 'Hello', '____'].join("\n"), :paragraph, :source, 'こんにちは']
+  )
+  def test_processor(data)
+    source, context, attr, expected = data
+    doc = Asciidoctor.load(source)
+    @processor.process_document(doc, @po, @new_po)
+    nodes = doc.find_by(context: context)
+    assert !nodes.empty?
+    assert_equal [expected] * nodes.size, nodes.map(&attr)
+  end
+
+  def test_simple_table
+    doc = Asciidoctor.load(['|===', '|Hello', '|==='].join("\n"))
+    @processor.process_document(doc, @po, @new_po)
+    table = doc.find_by(context: :table).first
+    assert_equal 'こんにちは', table.rows.body[0][0].text
+  end
+
+  def test_complex_table
+    doc = Asciidoctor.load("|===\na|* Hello\n|===\n")
+    @processor.process_document(doc, @po, @new_po)
+    table = doc.find_by(context: :table).first
+    assert_equal 'こんにちは', table.rows.body[0][0].inner_document.find_by.last.text
+  end
+
+  def test_no_translation
+    doc = Asciidoctor.load('foo bar')
+    @processor.process_document(doc, @po, @new_po)
+    paragraph = doc.find_by(context: :paragraph).first
+    assert_equal 'foo bar', paragraph.source
   end
 end
